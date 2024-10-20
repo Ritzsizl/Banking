26581f465c750431f7cba4526d99ac1517f5d4b0
Ritzsizl <ritzraha4u@gmail.com>
2024-10-20 15:00:09 +0530

    HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Ritam' };
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manageyour account and transactions efficiently"/>
+        </header>
+      </div>
+    </section>
   )
 }
 
